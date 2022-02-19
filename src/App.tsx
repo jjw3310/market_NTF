31913439bef95de508a5316cfeb655982ece8703
Clayton <jjw3310@igrev.kr>
2022-02-19 11:28:46 +0900

    Mint_metadata_merge_error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Main from "./routes/main";
 import MyAnimal from "./routes/my-animal";
-import SaleAnimal from "./routes/sale-animal";
+// import SaleAnimal from "./routes/sale-animal";
 
 
 const App: FC = () => {
@@ -36,10 +36,10 @@ const App: FC = () => {
             element={<Main account={account} />} />
           <Route path="/" element={<Main account={account} />} />
           <Route path="my-animal" element={<MyAnimal account={account} />} />
-          <Route
+          {/* <Route
             path="sale-animal"
             element={<SaleAnimal account={account} />}
-          />
+          /> */}
         </Routes>
       </Layout>
     </BrowserRouter>
